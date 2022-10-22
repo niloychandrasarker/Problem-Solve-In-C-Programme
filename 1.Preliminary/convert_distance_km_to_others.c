92b99b91442c2ad17cecdd862d59af13d8435e33
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-10-22 19:54:22 +0600

    convert_distance_km_to_others

---
+++ 1.Preliminary/convert_distance_km_to_others.c
@@ -0,0 +1,21 @@
+/*The distance between AUST main campus and Rajshahi campus (in km) is
+input through keyboard. Write a program to convert and print this distance
+in meters, feet, inches and centimeters. */
+
+#include<stdio.h>
+int main()
+{
+    float km, m, cm, f, in;
+    printf("Enter distance in kilometers: ");
+    scanf("%f", &km);
+    /* calculate the conversion */
+    m = km * 1000;
+    cm = km * 1000 * 100;
+    f = km * 3280.84;
+    in = km * 39370.08;
+    printf("The distance in Feet: %f\n", f);
+    printf("The distance in Inches: %f\n", in);
+    printf("The distance in Meters: %f\n", m);
+    printf("The distance in Centimeters: %f\n", cm);
+    return (0);
+}
