a8e7b0e6d2b038c2081a38c6365f1d543ea74f11
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-10-23 11:06:57 +0600

    gegorian calender

---
+++ 2.Conditional/5_gregorian_calender.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    while (1)
+    {
+    
+    int year,i,day=0;
+    printf("Enter year : ");
+    scanf("%d",&year);
+
+    for ( i = 1; i < year; i++)
+    {
+        if (i%4==0)
+        {
+            day=day+366;
+        
+        }
+        else
+        {
+            day=day+365;
+        }
+        
+    }
+    day=day % 7;
+    if(day==1)
+       printf("there is monday on 01/01/%d",year);
+    else if(day==2)
+       printf("there is tuesday on 01/01/%d",year);   
+    else if(day==3)
+       printf("there is wednesday on 01/01/%d",year);
+    else if(day==4)
+       printf("there is thursday on 01/01/%d",year);
+    else if(day==5)
+       printf("there is friday on 01/01/%d",year);
+    else if(day==6)
+       printf("there is saturday on 01/01/%d",year);
+    else if(day==0)
+       printf("there is sunday on 01/01/%d",year);  
+       else
+       printf("there is wrong entry"); 
+       printf("\n"); 
+    }        
+    return 0;
+}
