9943e749a69affad1420ad2e48fd380936f5dc54
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-10-23 19:23:14 +0600

    to_compute_n!_nCr_nPr

---
+++ 3.lopping/1_to_compute_n!_nCr_nPr.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int factorial(int);
+int combination(int, int);
+int permutation(int, int);
+
+int main()
+{
+    int n, r, c, p,fact;
+    printf("Enter the value of n :");
+    scanf("%d", &n);
+
+    printf("Enter the value of r :");
+    scanf("%d", &r);
+
+    c = combination(n, r);
+    p = permutation(n, r);
+    fact =factorial(n);
+    
+    printf("\nCombinations : %d", c);
+    printf("\nPermutations : %d", p);
+    printf("\nfactorial= %d",fact);
+    
+    return 0;
+}
+
+int combination(int n, int r)
+{
+    int c;
+
+    c = factorial(n) / (factorial(r) * factorial(n - r));
+
+    return c;
+}
+
+int permutation(int n, int r)
+{
+    int p;
+
+    p = factorial(n) / factorial(n - r);
+
+    return p;
+}
+
+int factorial(int n)
+{
+    int i;
+    int fact = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+   
+
+    return fact;
+    
+}
