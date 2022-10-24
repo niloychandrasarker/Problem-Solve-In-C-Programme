22bc3d8f0b13a5bb1c251e0e9b652040ec1554d7
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-10-24 10:18:13 +0600

    series2

--- 3.lopping/4_series2.c
+++ 3.lopping/4_series2.c
@@ -1,17 +1,18 @@
 /*Find out the sum of each of the following series. 
-(i) 3 + 11 + 19 + … + 1691.
+(ii) 7 + 20 + 33 + … ( up to 100 th term )
 */
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
     int i,sum=0;
        
-       for(i=3;i<=1691;i=i+8)
+       for(i=7;i<=100;i=i+13)
        {
-         printf("%d+",i);
+         
        sum=sum+i;
        } 
-        printf("\nsum=%d\n",sum);
+        
+        printf("\n7 + 20 + 33 + '…' 100=%d\n",sum);
     return 0;
 }
 
