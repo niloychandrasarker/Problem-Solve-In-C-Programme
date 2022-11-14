3d961bfeafd88fe3865d561ad037fa7348a851c8
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-11-14 09:41:42 +0600

    factorial

---
+++ 5.Function/1_factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int done(int);
+
+int main()
+{
+	int num, fact;
+	printf("Enter a number : ");
+	scanf("%d", &num);
+	fact = done(num);
+	printf("Factorial = %d\n", fact);
+	_getch();
+	return 0;
+}
+
+int done(int a)
+{
+	int fac = 1, i;
+	for (i = 1; i <= a; i++)
+		fac = fac*i;
+
+	return(fac);
+}
