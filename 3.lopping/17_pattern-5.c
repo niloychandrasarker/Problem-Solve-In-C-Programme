55d1f4a7b2c40d96fc38481a20ed24b05537a39d
Niloy Chandra Sarker <115910752+niloychandrasarker@users.noreply.github.com>
2022-11-15 21:07:40 +0600

    pattern

---
+++ 3.lopping/17_pattern-5.c
@@ -0,0 +1,25 @@
+/*write a programme to following this pattern
+Enter the N = 5
+* 
+* *
+* * *
+* * * *
+* * * * *
+*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n,row,col;
+    printf("Enter the N = ");
+    scanf("%d",&n);
+    for ( row = 1; row <= n; row++)
+    {
+        for ( col = 1; col <= row; col++)
+        {
+            printf("* ");
+        }
+      printf("\n");  
+    }
+    
+    return 0;
+}
